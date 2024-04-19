2b1b0ffcb24dbd042f2f11cd0474d7198cc4ef59
Neekzen <neekzen.62@gmail.com>
2024-04-19 10:51:27 +0200

    dnzdnzBHSYUSnèè254

--- backend/__tests__/submit.test.js
+++ backend/__tests__/submit.test.js
@@ -1,9 +1,9 @@
 const request = require("supertest");
 const { app, server } = require("../index.js");
 
-afterAll((done) => {
-    server.close(done);
-});
+    afterAll((done) => {
+        server.close(done);
+    });
 
 describe("POST /submit", () => {
     it("should respond with a success message", async () => {
@@ -11,7 +11,7 @@ describe("POST /submit", () => {
             .post("/submit")
             .send({ name: "Test Name", email: "test@example.com" });
 
-        expect(response.status).toBe(200);
-        expect(response.text).toBe("Form submitted successfully!");
+                        expect(response.status).toBe(200);
+                        expect(response.text).toBe("Form submitted successfully!");
     });
 });
